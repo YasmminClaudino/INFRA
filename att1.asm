0476b9148ba9b094b23fd2771ff36ba1390cc65d
YasmminClaudino <yasmmin.claudino@ufrpe.br>
2019-10-21 09:53:46 -0300

    att

--- att1.asm
+++ att1.asm
@@ -13,3 +13,22 @@ sw x5, 16(x8)
 addi x6, x0,5
 sw x6, 20(x8)
 
+li x9, 0x10010020
+lw x1, 0(x8)
+slli x1, x1,1
+sw x1,0(x9)
+lw x1, 4(x8)
+slli x1, x1,1
+sw x1,4(x9)
+lw x1, 8(x8)
+slli x1, x1,1
+sw x1,8(x9)
+lw x1, 12(x8)
+slli x1, x1,1
+sw x1,12(x9)
+lw x1, 16(x8)
+slli x1, x1,1
+sw x1,16(x9)
+lw x1, 20(x8)
+slli x1, x1,1
+sw x1,20(x9)
